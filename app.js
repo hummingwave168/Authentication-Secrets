df0afbd3390057e2a92bfdc7f961ef28be97a8d2
Prianshu <prianshu.parashar@hummingwave.com>
2022-09-16 12:02:49 +0530

    Cookies and Sessions PassportJS

--- app.js
+++ app.js
@@ -6,17 +6,31 @@ const ejs = require('ejs');
 
 // const encrypt = require('mongoose-encryption');
 // const md5 = require('md5');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
 
 const app = express();
 
 let port = process.env.port;
-if (port == null || port =="") {
+if (port == null || port == "") {
     port = 3000;
 }
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
+
+app.use(session({
+    secret: "Our Little Secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
@@ -27,16 +41,51 @@ const userSchema = new mongoose.Schema({
 
 // const secret = process.env.SECRET_STRING;
 // userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
-const saltRounds = 10;
+// const saltRounds = 10;
+userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model('User', userSchema);
+
+// Simplified Passport-Local Configuration
+passport.use(User.createStrategy());
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      cb(null, { id: user.id, username: user.username });
+    });
+  });
+  
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
 
 app.get("/", (req, res) => {
     res.render('home');
 });
 
+app.get("/secrets", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render('secrets');
+    } else {
+        res.redirect("/login");
+    }
+});
+
 app.get("/login", (req, res) => {
     res.render('login');
+});
+
+app.get("/logout", (req, res) => {
+    req.logout((err) => {
+        if (err) { 
+            console.log(err); 
+        }
+        res.redirect("/");
+    });
 });
 
 app.get("/register", (req, res) => {
@@ -46,41 +95,36 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
     const email = req.body.username;
     const password = req.body.password;
-    bcrypt.hash(password, saltRounds, (err, hash) => {
-        // Store hash in your password DB.
-        const user = new User({
-            email: email,
-            password: hash
-        });
-        user.save((err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.render('secrets');
-            }
-        });
-    });
-});
 
-app.post("/login", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    
-    User.findOne({email: username}, (err, response) => {
+    User.register({username: email}, password, (err, user) => {
         if (err) {
             console.log(err);
+            res.redirect("/register");
         } else {
-            bcrypt.compare(password, response.password, (err, result) => {
-                if (result === true) {
-                    res.render('secrets');
-                } else {
-                    res.send("<h1>Incorrect Credentials!!!</h1>");
-                }
+            passport.authenticate('local')(req, res, () => {
+                res.redirect("/secrets");
             });
         }
     });
 });
 
-app.listen(port, ()=> {
-    console.log("Server started on port " + port);
+app.post("/login", (req, res) => {
+    const user = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            passport.authenticate('local')(req, res, ()=> {
+                res.redirect("/secrets");
+            });
+        }
+    });
 });
+
+app.listen(port, () => {
+    console.log("Server started on port " + port);
+}); 
